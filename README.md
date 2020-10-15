08e312fabb5100b5d00aba00ff632fdc2b9934b1
ajitsinghkaler <ajitsinghkaler0@gmail.com>
2020-10-15 20:10:09 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# async-rxjs-typeahead
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/async-rxjs-typeahead)
---
+++ index.ts
@@ -0,0 +1,34 @@
+import { fromEvent, of } from "rxjs";
+import { ajax } from "rxjs/ajax";
+
+import {
+  debounceTime,
+  distinctUntilChanged,
+  map,
+  switchMap,
+  tap,
+  catchError,
+  filter
+} from "rxjs/operators";
+
+interface Country {
+  name: string;
+}
+
+const fakeContinentsRequest = keys =>
+  ajax
+    .getJSON<Country[]>(`https://restcountries.eu/rest/v2/name/${keys}`)
+    .pipe(catchError(() => of([{ name: "No countries found" }])));
+
+fromEvent(document.getElementById("type-ahead"), "keyup")
+  .pipe(
+    debounceTime(200),
+    map((e: any) => e.target.value),
+    filter(e => !!e),
+    tap(console.log),
+    distinctUntilChanged(),
+    switchMap(fakeContinentsRequest),
+    map(resp => resp.map(country => country.name)),
+    tap(c => (document.getElementById("output").innerText = c.join("\n")))
+  )
+  .subscribe();
